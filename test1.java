7617fe3fea1a4262942289dadb04906b0384156c
gaoguoqing2284 <294702344@qq.com>
2019-06-11 17:29:48 +0800

    这是今天的HelloWorld

---
+++ test1.java
@@ -0,0 +1,14 @@
+class HelloWrold{
+public static void main(String args[]){
+System.out.print("HelloWould1");
+}
+}
+class HelloWrold2{
+public static void main(String args[]){
+System.out.print("HelloWould2");
+}
+}
+
+
+
+
